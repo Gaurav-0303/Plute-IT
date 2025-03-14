16af0224618abbfe0106495f5caaf58dfc6773fd
Gaurav <gauravbodake1@gmail.com>
2025-03-14 15:14:14 +0530

    Dark mode toggle button added

--- app/src/main/java/com/plutecoder/itworld/fragments/ConnectFragment.kt
+++ app/src/main/java/com/plutecoder/itworld/fragments/ConnectFragment.kt
@@ -1,19 +1,35 @@
 package com.plutecoder.itworld.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.plutecoder.itworld.R
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.plutecoder.itworld.databinding.FragmentConnectBinding
+import com.plutecoder.itworld.views.isDarkModeEnabled
 
 class ConnectFragment : Fragment() {
+
+    private lateinit var binding: FragmentConnectBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_connect, container, false)
+    ): View {
+        binding = FragmentConnectBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonIsDark.isChecked = isDarkModeEnabled(requireContext())
+
+        binding.buttonIsDark.setOnCheckedChangeListener { _, isChecked ->
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            )
+        }
+    }
 }
