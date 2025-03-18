281810c67aa671b4d8bda0389e50234ab9148496
Gaurav <gauravbodake1@gmail.com>
2025-03-18 14:15:20 +0530

    Crash analytics implemented

--- app/src/main/java/com/plutecoder/itworld/MyApplication.kt
+++ app/src/main/java/com/plutecoder/itworld/MyApplication.kt
@@ -1,13 +1,21 @@
 package com.plutecoder.itworld
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.database.FirebaseDatabase
 
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
+
         // Enable Firebase Realtime Database disk persistence
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+
+        // Enable Crashlytics crash reporting
+        FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = true
     }
 }
